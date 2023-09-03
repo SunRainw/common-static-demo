0f4bc54e98c2f17676a97eb0b58a9d70ac732237
SunRainw <1285328994@qq.com>
2023-09-03 11:52:57 +0000

    update

---
+++ assets/HomeView-6df974dd.js
@@ -0,0 +1,1 @@
+import{_ as c,o as s,c as a,p as t,a as _,b as o}from"./index-e1d4125a.js";const n={},d=e=>(t("data-v-4c79a90a"),e=e(),_(),e),p={class:"home-container"},i=d(()=>o("div",{class:"text"},[o("h1",null,"动效demo & 技术实现"),o("p",null,"效果展示、实现分析、实时更新。")],-1)),r=[i];function l(e,h){return s(),a("div",p,r)}const u=c(n,[["render",l],["__scopeId","data-v-4c79a90a"]]);export{u as default};
