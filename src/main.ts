f75e4d40cfa58214f8ce6b15f391f5e72e0145ad
wangxuyu <wang_xuyu@uxsino.com>
2023-09-03 17:58:02 +0800

    fix: 开发环境修改

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+console.info("111", import.meta.env)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Components from "unplugin-vue-components/vite";
